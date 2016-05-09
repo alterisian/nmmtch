28f5bfcf2f8c150e8918baec1783ee7adc614063
Prelang Builder <builder@prelang.com>
2016-05-09 11:23:24 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Blocked' to allow for create, update

--- app/admin/blocked.rb
+++ app/admin/blocked.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Blocked do
+  permit_params :ip, :domain, :email
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
